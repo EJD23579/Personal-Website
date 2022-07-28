172a779192760d5213e9c376784595cd1862456a
= <=>
2022-07-28 17:59:56 +0100

    Added Javascript functionality for fade ins (proof of concept, more to come)

--- Script.js
+++ Script.js
@@ -3,6 +3,24 @@
 
 
 
+    var opacity = 0;
+    var intervalID = 0;
+
+    function fadeIn() {
+        intervalID = setInterval(show, 20);
 
 
+    }
+
+    function show() {
+        var python = document.getElementById("showPy");
+        opacity = Number(window.getComputedStyle(python).getPropertyValue("opacity"));
+        if (opacity <1) {
+            opacity = opacity + 0.1;
+            python.style.opacity = opacity;
+        }
+        else {
+            clearInterval(intervalID);
+        }
+    }
 
