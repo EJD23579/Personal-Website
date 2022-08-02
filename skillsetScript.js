b945489468fb73eb1f6f00464dc06d6aa54a6de7
= <=>
2022-08-02 15:51:50 +0100

    Major functionality changes to all pages. Javascript, React and Jquery interactivity added as well as CSS webkit animations.

--- skillsetScript.js
+++ skillsetScript.js
@@ -2,6 +2,9 @@
 
 
 
+ // SKillsets JavaScript START
+
+
     // Declare opacity and time variables for fade ins
     var opacity = 0;
     var intervalID = 0;
@@ -87,6 +90,68 @@ function showPSQL() {
     if (opacity < 1) {
         opacity = opacity + 0.1;
         psql.style.opacity = opacity;
+    }
+    else {
+        clearInterval(intervalID);
+    }
+}
+
+// C# fade in settings
+// Assign interval ID value based on 20 millisecond intervals using show function
+function fadeInCsh() {
+    intervalID = setInterval(showCsh, 20);
+
+
+}
+// Show C# text
+function showCsh() {
+    var csh = document.getElementById("showCsh");
+    opacity = Number(window.getComputedStyle(csh).getPropertyValue("opacity"));
+    if (opacity < 1) {
+        opacity = opacity + 0.1;
+        csh.style.opacity = opacity;
+    }
+    else {
+        clearInterval(intervalID);
+    }
+}
+
+// .Net fade in settings
+// Assign interval ID value based on 20 millisecond intervals using show function
+function fadeInDotNet() {
+    intervalID = setInterval(showDotNet, 20);
+
+
+}
+// Show .Net text
+function showDotNet() {
+    var dotNet = document.getElementById("showDotNet");
+    opacity = Number(window.getComputedStyle(dotNet).getPropertyValue("opacity"));
+    if (opacity < 1) {
+        opacity = opacity + 0.1;
+        dotNet.style.opacity = opacity;
+    }
+    else {
+        clearInterval(intervalID);
+    }
+}
+
+
+
+// React fade in settings
+// Assign interval ID value based on 20 millisecond intervals using show function
+function fadeInReact() {
+    intervalID = setInterval(showReact, 20);
+
+
+}
+// Show React text
+function showReact() {
+    var react = document.getElementById("showReact");
+    opacity = Number(window.getComputedStyle(react).getPropertyValue("opacity"));
+    if (opacity < 1) {
+        opacity = opacity + 0.1;
+        react.style.opacity = opacity;
     }
     else {
         clearInterval(intervalID);
